4fdecc78b398335f7c517c1f4873bb80e8ea79e4
Alex Palacios <alexis1994662@icloud.com>
2023-02-13 22:01:31 -0700

    "removed commas"

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-44-238-37-29.us-west-2.compute.amazonaws.com','0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-44-238-37-29.us-west-2.compute.amazonaws.com',
+                 '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
